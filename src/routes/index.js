d4d9b16259cee820f935d0f5ec924033cc383ea0
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 10:51:36 -0300

    criando a pagina da rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,20 @@ import Home from "../pages/Home";
 import Singup from "../pages/Singup";
 import Signin from "../pages/Signin";
 
+const Private = ({ Item }) => {
+  const signed = false;
+  return signed > 0 ? <Item /> : <Signin />;
+};
+
 const RoutesApp = () => {
   return (
     <BrowserRouter>
       <Fragment>
         <Routes>
-          <Route></Route>
+          <Route exact path="/Home" element={<Private Item={Home} />} />
+          <Route path="/" element={<Signin />} />
+          <Route exact path="/signup" element={<Singup />} />
+          <Route path="*" element={<Signin />} />
         </Routes>
       </Fragment>
     </BrowserRouter>
