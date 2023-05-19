ec3b10d9881dc55a78d1889ea3cc10c35b784dda
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 10:29:38 -0300

    criando o body

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,5 +5,13 @@ const GlobalStyle = createGlobalStyle`*
     margin:0;
     padding:0;
     box-sizing:border-box;
-}`;
+}
+    body{
+        width:100vw;
+        height:100vh;
+        background-color:#f0f2f5;
+        font-family:Arial, Helvetica, sans-serif;
+    }
+
+`;
 export default GlobalStyle;
