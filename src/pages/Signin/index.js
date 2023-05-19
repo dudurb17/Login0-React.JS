324371b6f9acb3b8c6c07f497a2c8472e9785102
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 16:56:15 -0300

    pausa para arrumar o erro do JSON

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,34 +1,43 @@
-import React, { useState } from "react";
-import * as C from "./styles";
-import Input from "../../components/Input";
-import Button from "../../components/button";
-import { Link, useNavigate } from "react-router-dom";
-import useAuth from "../../hooks/useAuth";
+// import React, { useState } from "react";
+// import * as C from "./styles";
+// import Input from "../../components/Input";
+// import Button from "../../components/button";
+// import { Link, useNavigate } from "react-router-dom";
+// import useAuth from "../../hooks/useAuth";
 
-const Signin = () => {
-  const [email, setEmail] = useState();
-  const [senha, setSenha] = useState();
-  const [error, setError] = useState();
+// const Signin = () => {
 
-  return (
-    <C.Container>
-      <C.Label>Sistema de Login</C.Label>
-      <C.Content>
-        <Input
-          type="email"
-          placeholder="Digite seu email"
-          value={email}
-          onChange={(e) => [setEmail(e.target.value), setError("")]}
-        />
-        <Input
-          type="password"
-          placeholder="Digite sua senha"
-          value={senha}
-          onChange={(e) => [setSenha(e.target.value), setError("")]}
-        />
-      </C.Content>
-    </C.Container>
-  );
-};
+//   const [email, setEmail] = useState();
+//   const [senha, setSenha] = useState();
+//   const [error, setError] = useState();
 
-export default Signin;
+//   return (
+//     <C.Container>
+//       <C.Label>Sistema de Login</C.Label>
+//       <C.Content>
+//         <Input
+//           type="email"
+//           placeholder="Digite seu email"
+//           value={email}
+//           onChange={(e) => [setEmail(e.target.value), setError("")]}
+//         />
+//         <Input
+//           type="password"
+//           placeholder="Digite sua senha"
+//           value={senha}
+//           onChange={(e) => [setSenha(e.target.value), setError("")]}
+//         />
+//         <C.labelError>{error}</C.labelError>
+//         <Button text="Entrar" onClick={handleLogin} />
+//         <C.LabelSignup>
+//           Não tem uma conta?
+//           <C.Strong>
+//             <Link to="/singup">&nbsp; Registre-se</Link>
+//           </C.Strong>
+//         </C.LabelSignup>
+//       </C.Content>
+//     </C.Container>
+//   );
+// };
+
+// export default Signin;
