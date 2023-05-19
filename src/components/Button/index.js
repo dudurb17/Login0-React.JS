91afb94e279e866d88865d7811be2c5b8697fc87
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 16:25:26 -0300

    button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,8 +1,12 @@
 import React from "react";
 import * as C from "./styles";
 
-const Button = ({ text, onClik, Type = "button" }) => {
-  return <C.Button></C.Button>;
+const Button = ({ text, onClick, Type = "button" }) => {
+  return (
+    <C.Button type={Type} onClick={onClick}>
+      {text}
+    </C.Button>
+  );
 };
 
 export default Button;
