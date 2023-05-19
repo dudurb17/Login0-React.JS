23f1c08dba15f1ceb246d60535929e9a550080ce
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 16:49:22 -0300

    adc os inputs

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import * as C from "./styles";
+import Input from "../../components/Input";
+import Button from "../../components/button";
+import { Link, useNavigate } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
 
 const Signin = () => {
-  return <div>Signin</div>;
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const [error, setError] = useState();
+
+  return (
+    <C.Container>
+      <C.Label>Sistema de Login</C.Label>
+      <C.Content>
+        <Input
+          type="email"
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => [setEmail(e.target.value), setError("")]}
+        />
+        <Input
+          type="password"
+          placeholder="Digite sua senha"
+          value={senha}
+          onChange={(e) => [setSenha(e.target.value), setError("")]}
+        />
+      </C.Content>
+    </C.Container>
+  );
 };
 
 export default Signin;
