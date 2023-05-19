b36fff11101412bebdb4c6b5bdd0083e1488179e
dudurb17 <eduardo.robettibedin@gmail.com>
2023-05-19 16:41:14 -0300

    strong style

--- src/pages/Signin/styles.js
+++ src/pages/Signin/styles.js
@@ -35,3 +35,11 @@ export const labelError = styled.label`
   font-size: 14px;
   color: red;
 `;
+
+export const Strong = styled.strong`
+  cursor: pointer;
+  a {
+    text-decoration: none;
+    color: #676767;
+  }
+`;
